3556b5235a0fa6c62f78cf7e17e2ee5d3e05721d
Paco Ibanez-Castillo <fraibacas@gmail.com>
2016-03-17 20:48:25 +0000

    update module name

--- remove_tx_docs.py
+++ remove_tx_docs.py
@@ -5,15 +5,15 @@ dmd = ZenScriptBase(connect=True).dmd
 import zope.component
 from Products.ZenUtils.GlobalConfig import getGlobalConfiguration
 from Products.Zuul.catalog.model_catalog import TX_SEPARATOR
-from zenoss.modelindex.searcher import SearchParams
+from zenoss.modelindex.model_index import SearchParams
 
 def remove_temp_transaction_docs():
     config = getGlobalConfiguration()
-    solr_servers = config.get('solr-servers', '10.87.128.105:8984')
+    solr_servers = config.get('solr-servers', 'localhost:8983')
     uid = "*{0}*".format(TX_SEPARATOR)
     query = {"uid" : uid}
     search_params=SearchParams(query)
-    searcher = zope.component.createObject('ModelSearcher', solr_servers)
+    searcher = zope.component.createObject('ModelIndex', solr_servers)
     searcher.unindex_search(search_params)
 
 if __name__ == "__main__":
